b6c6230ae08ca05c0cff80483cc66bf331005ca8
cdashwood <45880107+cdashwood@users.noreply.github.com>
2021-05-19 11:49:03 +0100

    Update Fig3.m

--- Fig3.m
+++ Fig3.m
@@ -1,4 +1,4 @@
-set(0, 'defaultaxesfontsize', 8, 'defaulttextfontsize', 8)
+set(0, 'defaultaxesfontsize', 4, 'defaulttextfontsize', 4)
 figure(3, 'position', [0 0 1000 400])
 colours_pi = autumn(6);
 
